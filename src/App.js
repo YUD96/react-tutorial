8486a2607e0a0e90d5caf1bd990ae87f66ba6a72
YUD96 <yudai000916@gmail.com>
2023-12-17 17:00:15 +0900

    着手順のエラー解消

--- src/App.js
+++ src/App.js
@@ -56,7 +56,7 @@ export default function Game() {
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
   const currentSquares = history[currentMove];
-  const setXIsNext = currentMove % 2 === 0;
+  const xIsNext = currentMove % 2 === 0;
 
   function handlePlay(nextSquares) {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
